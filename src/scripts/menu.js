b5fcd925af724cac73af7372c2937106805b4802
jonr8 <jon@toplane.co.uk>
2024-02-14 18:05:03 +0000

    Menu changes

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,5 +1,9 @@
 document.addEventListener('astro:page-load', () => {
   document.querySelector('.hamburger').addEventListener('click', () => {
-    document.querySelector('.nav-links').classList.toggle('expanded');
+    if (document.querySelector('.nav-links').classList.contains('invisible')) {
+      document.querySelector('.nav-links').classList.replace('invisible','visible');
+    } else {
+    document.querySelector('.nav-links').classList.replace('visible','invisible');
+  }
   });
 });
