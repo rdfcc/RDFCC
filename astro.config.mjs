bdcf0f9c0fa14679df28a3a94babd79f2427a512
rdfcc <rdfccwebmaster@gmail.com>
2024-03-18 11:39:07 +0000

    Added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import preact from "@astrojs/preact";
 import tailwind from "@astrojs/tailwind";
-
 import mdx from "@astrojs/mdx";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://rdfcc.netlify.app/",
-  integrations: [preact(), tailwind(), mdx()]
+  site: "https://rdfcc.org.uk/",
+  integrations: [preact(), tailwind(), mdx(), sitemap()]
 });
